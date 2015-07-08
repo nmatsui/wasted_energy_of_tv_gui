19a7dad3cf12ff89949d0b8d75dafa4d5817f7ab
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2015-07-08 06:10:18 +0000

    handle picked_date

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(function() {
-  $("#target-date").datepicker({
+  $.isBlank = function(obj) {
+    return (!obj || $.trim(obj) === "");
+  };
+
+  $("#target_date").datepicker({
     format: "yyyy/mm/dd",
     orientation: "top auto",
     autoclose: true,
     todayHighlight: true
+  });
+
+  $("#datepick_form").submit(function() {
+    var picked = $("#target_date").val();
+    if (!($.isBlank(picked))) {
+      console.log(picked);
+    }
+    return true;
   });
 });
