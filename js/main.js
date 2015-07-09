78918a24eb46ec0928f1189525d2bc74aa98b7d0
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2015-07-09 05:52:49 +0000

    add canvas logic

--- js/main.js
+++ js/main.js
@@ -3,18 +3,57 @@ $(function() {
     return (!obj || $.trim(obj) === "");
   };
 
-  $("#target_date").datepicker({
+  $(".datepick_form .target_date").datepicker({
     format: "yyyy/mm/dd",
     orientation: "top auto",
     autoclose: true,
     todayHighlight: true
   });
 
-  $("#datepick_form").submit(function() {
-    var picked = $("#target_date").val();
-    if (!($.isBlank(picked))) {
-      console.log(picked);
+  $(".datepick_form").submit(function() {
+    console.log(".datepick_form submit");
+    console.log($(this).find(".target_date").val());
+    var pickedDate = $(this).find(".target_date").val();
+    if (!($.isBlank(pickedDate))) {
+      console.log("pickedDate=" + pickedDate);
+      watchedList = getWatchedList(pickedDate);
+      wrapper = $(this).find(".chart")
+      canvas = $(this).find(".chart canvas")
+      showChart(wrapper, canvas, watchedList);
     }
-    return true;
+    return false;
   });
+
+  function getWatchedList(targetDate) {
+    console.log("getWatchedList");
+    return testData;
+  }
+
+  function showChart(wrapper, canvas, watchedList) {
+    console.log("showChart");
+    var w = wrapper.width();
+    var h = wrapper.height();
+    console.log("w:" + w + ":h:" + h);
+
+    canvas.attr({"width":w, "height":h});
+    canvas.width(w);
+    canvas.height(h);
+
+    canvas.clearCanvas();
+
+    var uw = Math.round(w/watchedList.length);
+    console.log(uw);
+
+    $.each(watchedList, function(i, item) {
+      canvas.drawRect({
+        fillStyle: "rgb(" + i + "," + i + "," + i + ")",
+        x: uw * i,
+        y: 0,
+        width: uw,
+        height: h,
+        fromCenter: false
+      }); 
+    });
+
+  }
 });
