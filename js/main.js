dfd7832297a4efc27528b01d4638aca8530a893b
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2015-07-10 04:55:50 +0000

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(function() {
+  "use strict";
   var form_cnt = 1;
+
+  function insertFunc(element) {
+    element.on("submit", showChart)
+           .find(".remove_form_btn").on("click", removeForm);
+    element.find(".target_date").datepicker({
+             format: "yyyy/mm/dd",
+             orientation: "top auto",
+             autoclose: true,
+             todayHighlight: true
+           });
+  }
 
   insertFunc($(".datepick_form"));
 
@@ -23,26 +35,15 @@ $(function() {
     insertFunc($(clone));
   });
 
-  function insertFunc(element) {
-    element.on("submit", showChart)
-           .find(".remove_form_btn").on("click", removeForm);
-    element.find(".target_date").datepicker({
-             format: "yyyy/mm/dd",
-             orientation: "top auto",
-             autoclose: true,
-             todayHighlight: true
-           });
-  }
-
   function showChart(e) {
     console.log("showChart");
-    console.log($(this).find(".target_date").val());
-    var pickedDate = $(this).find(".target_date").val();
-    if (pickedDate && $.trim(pickedDate) != "") {
+    console.log($(e.target).find(".target_date").val());
+    var pickedDate = $(e.target).find(".target_date").val();
+    if (pickedDate && $.trim(pickedDate) !== "") {
       console.log("pickedDate=" + pickedDate);
-      watchedList = getWatchedList(pickedDate);
-      wrapper = $(this).find(".chart")
-      canvas = $(this).find(".chart canvas")
+      var watchedList = getWatchedList(pickedDate);
+      var wrapper = $(e.target).find(".chart");
+      var canvas = $(e.target).find(".chart canvas");
       drawChart(wrapper, canvas, watchedList);
     }
     return false;
@@ -62,7 +63,6 @@ $(function() {
     console.log("showChart");
     var w = wrapper.width();
     var h = wrapper.height();
-    console.log("w:" + w + ":h:" + h);
 
     canvas.attr({"width":w, "height":h});
     canvas.width(w);
@@ -71,10 +71,8 @@ $(function() {
     canvas.clearCanvas();
 
     var uw = Math.round(w/watchedList.length);
-    console.log(uw);
 
     $.each(watchedList, function(i, item) {
-      console.log(item);
       canvas.drawRect({
         fillStyle: decideColor(item),
         x: uw * i,
@@ -88,7 +86,7 @@ $(function() {
 
   function decideColor(item) {
     var hue;
-    if (item.count == 0) {
+    if (item.count === 0) {
       return "hsl(0, 0%, 0%)";
     }
     else {
