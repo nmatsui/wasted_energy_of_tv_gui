7797ecb18229dedc23318df117a00e07fa4334dc
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2015-07-12 13:24:48 +0900

    handle responsive

--- js/app/chart.js
+++ js/app/chart.js
@@ -10,6 +10,44 @@ define(["jquery", "jcanvas"], function($) {
     }
   }
 
+  function getOffset(w, h, i) {
+    var offset = {};
+    var result = {};
+    if (720 <= w) {
+      offset = {x1:0, y1:0, x2:0, y2:0, x3:0, y3:0, x4:0, y4:0};
+      result.h = h/2;
+      result.m = (w - 720)/2;
+    }
+    else if (360 <= w && w < 720) {
+      offset = {x1:0, y1:0, x2:0, y2:0, x3:-360, y3:h/2, x4:-360, y4:h/2};
+      result.h = h/4;
+      result.m = (w - 360)/2;
+    }
+    else {
+      offset = {x1:0, y1:0, x2:-180, y2:h/4, x3:-360, y3:h/2, x4:-540, y4:h*3/4};
+      result.h = h/8;
+      result.m = (w - 180)/2;
+    }
+
+    if (0 <= i && i < 180) {
+      result.x = offset.x1;
+      result.y = offset.y1;
+    }
+    else if (180 <= i && i < 360) {
+      result.x = offset.x2;
+      result.y = offset.y2;
+    }
+    else if (360 <= i && i < 540) {
+      result.x = offset.x3;
+      result.y = offset.y3;
+    }
+    else {
+      result.x = offset.x4;
+      result.y = offset.y4;
+    }
+    return result;
+  }
+
   return {
     clearChart: function($wrapper, $canvas) {
       console.log("clearChart");
@@ -38,17 +76,19 @@ define(["jquery", "jcanvas"], function($) {
       console.log("showChart");
       var w = $canvas.width();
       var h = $canvas.height();
-      var uw = 1;
+      console.log("w:" + w + " h:" + h);
 
       $canvas.clearCanvas();
 
       $.each(watchedList, function(i, item) {
+        var offset = getOffset(w, h, i);
+
         $canvas.drawRect({
           fillStyle: decideColor(item),
-          x: uw * i,
-          y: 0,
-          width: uw,
-          height: h/2,
+          x: i + offset.x + offset.m,
+          y: 0 + offset.y,
+          width: 1,
+          height: offset.h,
           fromCenter: false
         }); 
         if (i % 30 === 0) {
@@ -56,8 +96,8 @@ define(["jquery", "jcanvas"], function($) {
           $canvas.drawLine({
             strokeStyle: color,
             strokeWidth: 1,
-            x1: uw * i, y1: h/2 - 5,
-            x2: uw * i, y2: h/2 + 5
+            x1: i + offset.x + offset.m, y1: offset.y + offset.h - 5,
+            x2: i + offset.x + offset.m, y2: offset.y + offset.h + 5
           });
           $canvas.drawText({
             fillStyle: color,
@@ -65,8 +105,8 @@ define(["jquery", "jcanvas"], function($) {
             fontFamily: "Arial",
             align: "left",
             respectAlign: true,
-            x: uw * i,
-            y: h/2 + 15,
+            x: i + offset.x + offset.m,
+            y: offset.y + offset.h + 15,
             text: "" + i / 30 + "時"
           });
         }
